ccafda508a4bd2497c4b7db18556f874a85df24a
JenswBE <6514515+JenswBE@users.noreply.github.com>
2026-08-27 14:38:11 +0000

    Add Nix flake support

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "A simple fullscreen clock";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          packages.default = pkgs.buildGoModule {
+            pname = "clock";
+            version = "0.1.0";
+
+            src = ./.;
+
+            # Set this to the hash reported by Nix if dependencies need
+            # to be fetched. null means don't use vendored dependencies.
+            vendorHash = "sha256-SLZuC4ImkgrthuPOHxJ/PJadDEdJFAHCghVCtZy681s=";
+
+            subPackages = [ "." ];
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              makeWrapper
+            ];
+
+            buildInputs = with pkgs; [
+              # OpenGL
+              libGL
+
+              # X11
+              libX11
+              libxcursor
+              libxi
+              libxinerama
+              libxrandr
+              libxxf86vm
+
+              # Keyboard/input
+              libxkbcommon
+
+              # Wayland
+              wayland
+            ];
+
+            postInstall = ''
+              wrapProgram "$out/bin/clock" \
+                --prefix PATH : ${pkgs.lib.makeBinPath [
+                  pkgs.brightnessctl
+                ]}
+            '';
+
+            ldflags = [
+              "-s"
+              "-w"
+            ];
+
+            meta = {
+              mainProgram = "clock";
+              description = "A simple fullscreen clock";
+              platforms = [ "x86_64-linux" ];
+            };
+          };
+        }
+      );
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module fullscreen-clock
+module clock
 
 go 1.26
 
-require fyne.io/fyne/v2 v2.8.0
+require fyne.io/fyne/v2 v2.7.4
 
 require (
 	fyne.io/systray v1.12.2 // indirect
@@ -18,6 +18,7 @@ require (
 	github.com/fyne-io/image v0.1.1 // indirect
 	github.com/fyne-io/oksvg v0.2.0 // indirect
 	github.com/go-gl/gl v0.0.0-20260331235117-4566fea9a276 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
 	github.com/go-gl/glfw/v3.4/glfw v0.1.0-pre.1.0.20260707082822-2a407d02d01a // indirect
 	github.com/go-text/render v0.2.1 // indirect
 	github.com/go-text/typesetting v0.3.4 // indirect
